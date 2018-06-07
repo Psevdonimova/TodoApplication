99fb2396f88dd08e9be91c6144ad576c16173cd0
Psevdonimova <psevdonimova7@gmail.com>
2018-06-07 04:33:47 +0300

    Separate library called TodoRazorCoreDomain was created and contains all our domain models

--- TodoRazorCoreApp/Controllers/HomeController.cs
+++ TodoRazorCoreApp/Controllers/HomeController.cs
@@ -13,52 +13,14 @@ namespace TodoRazorCoreApp.Controllers
     public class HomeController : Controller
     {
         private readonly ITaskContext _context;
-        static private int count = 0;
 
         public HomeController(ITaskContext context)
         {
             _context = context;
         }
 
-        //[HttpGet]
         public IActionResult Index()
         {
-            /*_context.AddNewTask(new Task()
-            {
-                Title = "Title",
-                Description = "Description",
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now,
-            });
-            _context.AddNewTask(new Task()
-            {
-                Title = "Title",
-                Description = "Description",
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now,
-            });
-            _context.AddNewTask(new Task()
-            {
-                Title = "Title",
-                Description = "Description",
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now,
-            });*/
-
-            ViewBag.Tasks = _context.GetRange(count, 0);
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Create(Task task)
-        {
-            count++;
-            _context.AddNewTask(task);
             return View();
         }
     }
